315af3c0605799fae4fe9109103075782b704e35
Doug Hardman <59294403+doughardman1@users.noreply.github.com>
2023-09-04 21:07:21 +0100

    beginning REM code

---
+++ river_elevation_model.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import osmnx as ox
+
+from shapely.geometry import box
+from scipy.spatial import cKDTree as KDTree 
+
+import xarray as xr
+import xrspatial
+import rioxarray
+
+import matplotlib.pyplot as plt
